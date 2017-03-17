960f366e951409e65480434fa4766dc595e606d1
zhangzhitang <zhangzhitang@daheng.co>
2017-03-17 16:01:55 +0800

    删除router

--- routers/api/index.js
+++
@@ -1,9 +0,0 @@
-// var router = require('koa-router')()
-// var md_router = require('../mdrouter')
-
-// router.use('/markdown', md_router.routers(), md_router.allowedMethods())
-
-// module.exports = router
-
-// import path from 'path'
-// import { readdirSync } from 'fs'
--- routers/mdrouter.js
+++
@@ -1,8 +0,0 @@
-const router = require('koa-router')();
-
-const md_controller = require('../controllers/mdcontroller');
-
-router.get('/index', md_controller.getMd);
-router.post('/save', md_controller.saveMd);
-
-module.exports = router;
